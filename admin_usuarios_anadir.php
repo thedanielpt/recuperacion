f84526e64de27b820ef5d82a8f446356e8e446fa
Daniel Pamies Teruel <thedanielpt@gmail.com>
2025-06-10 18:06:11 +0200

    Ya se puede insertar

--- admin_usuarios_anadir.php
+++ admin_usuarios_anadir.php
@@ -8,24 +8,58 @@
     $contrasena = isset($_POST['contrasena']) ? $_POST['contrasena'] : null;
     $contrasena_repetir = isset($_POST['repetir_contrasena']) ? $_POST['repetir_contrasena'] : null; 
     $rol = isset($_POST['usuario']) ? $_POST['usuario'] : null;
+    $nombre = isset($_POST['nombre_apellidos']) ? $_POST['nombre_apellidos'] : null;
+    $alta = isset($_POST['alta']) ? $_POST['alta'] : null;
+    $curso = isset($_POST['curso']) ? $_POST['curso'] : null;
+    $alergias = isset($_POST['alergias']) ? $_POST['alergias'] : null;
+    $usuarioRepetidoEnLaBD = null;
 
-    if(!empty($_POST['crear'])) {
+    try {
+        if(!empty($_POST['crear'])) {
         
-        //Query que se va a ejecutar
-        $sql = ("INSERT INTO usuario (email, password, rol) VALUES (:email, :password, :rol)");
+            //Query que se va a ejecutar
+            $sql = ("INSERT INTO usuario (email, password, rol) VALUES (:email, :password, :rol)");
 
-        //parametros que se quiere pasar
-        $param = ['rol' => $rol,
-        'password'=> $contrasena,
-        'email' => $email];
+            //parametros que se quiere pasar
+            $param = ['rol' => $rol,
+            'password'=> $contrasena,
+            'email' => $email];
 
-        //Prepara la consulta
-        $stmt = $pdo->prepare($sql);
-        
-        //ejecuta la consulta
-        $stmt->execute($param);
+            //Prepara la consulta
+            $stmt = $pdo->prepare($sql);
+            
+            //ejecuta la consulta con los parametros
+            $stmt->execute($param);
 
+            //Si el rol es Alumno se mete aquí para crear en la tabla alumno un alumno
+            if($rol == "Alumno"){
+                //Query que se va a ejecutar
+                $sql_alumo = ("INSERT INTO alumno (email, nombre, alta, curso) VALUES (:email, :nombre, :alta, :curso)");
+
+                //parametros para que se ejecute
+                $param_alumno = [ 'email' => $email,
+                'nombre' => $nombre,
+                'alta' => $alta,
+                'curso' => $curso
+                ];
+
+                //Prepara la query
+                $stmt = $pdo->prepare($sql_alumo);
+
+                //Ejecuta la query con los parametros
+                $stmt->execute($param_alumno);
+
+                /*if ($alergias != null) {
+                    foreach($alergias as $alergia) {
+                        $sql_alumo_alergias = ("INSERT INTO bocadillos_laergenos () VALUES ")
+                    }
+                }*/
+            }
+        }
+    } catch ( PDOException $e) {
+        $usuarioRepetidoEnLaBD = "No se pueden repetir el email";
     }
+    
 ?>
 
 <!DOCTYPE html>
@@ -40,16 +74,19 @@
             var nombre_apellido = document.getElementById('nombre_apellido');
             var curso = document.getElementById('curso');
             var alergenos = document.getElementById('alergenos');
+            var alta = document.getElementById('alta');
             var usuario = document.getElementById('usuario').value;
 
             if (usuario != "Alumno") {
                 nombre_apellido.style.display = "none";
                 curso.style.display = "none";
                 alergenos.style.display = "none";
+                alta.style.display = "none";
             } else {
                 nombre_apellido.style.display = "block";
                 curso.style.display = "block";
                 alergenos.style.display = "block";
+                alta.style.isplay = "block";
             }
         }
     </script>
@@ -65,8 +102,6 @@
                 <li><a href="admin_usuarios.php">Usuarios</a></li>
             </ul>
         </nav>
-
-        <?php echo isset($_POST['crear']); ?>
 
         <div id="div_logo">
             <img src="img/login-logo.png" id="img_logo">
@@ -94,9 +129,9 @@
 
                 
                 <div class="formulario_div" id="nombre_apellido">
-                    <label for="Nombre_apellidos_usuario" class="label_crear_modificar">Nombre y apellidos del usuario:</label>
+                    <label for="nombre_apellidos" class="label_crear_modificar">Nombre y apellidos del usuario:</label>
                     <div class="div_input_crear">
-                        <input type="text" name="Nombre_apellidos_usuario" placeholder="Daniel Pamies Teruel" class="input_crear">
+                        <input type="text" name="nombre_apellidos" placeholder="Daniel Pamies Teruel" class="input_crear">
                     </div>
                 </div>
     
@@ -117,45 +152,26 @@
                     <div class="div_input_crear">
                         <input type="email" name="email" placeholder="daniel@elCampico.com" class="input_crear" required>
                     </div>
+                    <p><?php echo $usuarioRepetidoEnLaBD; ?></p>
                 </div>
     
-                <!--<div class="formulario_div">
+                <div class="formulario_div">
                     <label for="contrasena" class="label_crear_modificar">Contraseña:</label>
                     <div class="div_input_crear">
-                        <input type="text" name="contrasena" placeholder="pepito_123" class="input_crear" required>
-                    </div>
-                    <p><?php //$prueba ?></p>
-                </div>-->
-
-                <div class="formulario_div">
-                    <label for="repetir_contrasena" class="label_crear_modificar">Repetir la contraseña:</label>
-                    <div class="div_input_crear">
-                        <input type="text" name="repetir_contrasena" placeholder="pepito_123" class="input_crear" required>
+                        <input type="password" name="contrasena" placeholder="pepito_123" class="input_crear" required>
                     </div>
                 </div>
-        
-                <div class="formulario_div" id="alergenos">
-                    <label for="alergias" class="label_crear_modificar">Alergias:</label>
-                    <select name="alergias" multiple class="seleccion">
-                        <option value="">No tiene alergenos</option>
-                        <?php
-                        //SQL que se va a ejecutar
-                            $sql = ('SELECT *
-                            FROM alergenos');
-                            //Se prepara la sql
-                            $stmt = $pdo->prepare($sql);
-                            //Ejecuta la consulta
-                            $stmt->execute();
-                            $alergias = $stmt->fetchAll();
-                            foreach($alergias as $alergia){
-                                echo '<option value='.$alergia['id'].'>'.$alergia['nombre'].'</option>';
-                            }
-                        ?>
+
+                <div class="formulario_div" id="alta">
+                    <label for="alta" class="label_crear_modificar">Dado de alta o e baja:</label>
+                    <select name="alta" id="alta" class="seleccion">
+                        <option value="false" default>Baja</option>
+                        <option value="true">Alta</option>
                     </select>
                 </div>
-        
+
                 <div id="div_boton_crear">
-                    <button type="submit" name="crear" class="boton_crear_modificar" value="1">Crear usuario</button>
+                        <button type="submit" name="crear" class="boton_crear_modificar" value="1">Crear usuario</button>
                 </div>
             </form>
         </article>
